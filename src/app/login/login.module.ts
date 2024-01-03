2222d0ee10b4a879cb7933c2830125f2fa4950b2
RashidKhan04 <142685805+RashidKhan04@users.noreply.github.com>
2024-01-03 22:46:20 +0500

    Team Collaboration updates

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,26 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { IgxButtonModule, IgxRippleModule, IgxIconModule, IgxInputGroupModule, IgxCheckboxModule } from 'igniteui-angular';
-import { FormsModule } from '@angular/forms';
 import { CreateAccountComponent } from './create-account/create-account.component';
+import { IgxInputGroupModule, IgxButtonModule, IgxRippleModule, IgxIconModule, IgxCheckboxModule } from 'igniteui-angular';
+import { FormsModule } from '@angular/forms';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    SignInComponent,
-    CreateAccountComponent
+    CreateAccountComponent,
+    SignInComponent
   ],
   imports: [
     CommonModule,
     LoginRoutingModule,
+    IgxInputGroupModule,
     IgxButtonModule,
     IgxRippleModule,
+    FormsModule,
     IgxIconModule,
-    IgxInputGroupModule,
-    IgxCheckboxModule,
-    FormsModule
+    IgxCheckboxModule
   ]
 })
 export class LoginModule {
